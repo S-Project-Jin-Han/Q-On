34b1bfa6d76fb5364f0dcab06af536771e7cfb83
pillow12360 <ehdcks1256@naver.com>
2025-08-19 13:56:07 +0900

    #15 refactor: zod를 통해 환경변수 스키마 검증 로직 적용 supabase utils

--- src/shared/lib/supabase/supabase-server.ts
+++ src/shared/lib/supabase/supabase-server.ts
@@ -1,29 +1,22 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import { env } from '@/shared/lib/env';
 
 export const supabaseServer = async () => {
   const cookieStore = await cookies();
 
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
+  return createServerClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
       },
-    }
-  );
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+        } catch {
+          // 미들웨어에서 세션 갱신 시 호출되는 메서드
+        }
+      },
+    },
+  });
 };
