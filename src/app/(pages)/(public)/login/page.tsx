8704fb08c640f36bf6f016980b5c667bb88b70dd
pillow12360 <ehdcks1256@naver.com>
2025-08-22 16:55:15 +0900

    #31 chore: 페이지 컴포넌트 명 수정

--- src/app/(pages)/(public)/login/page.tsx
+++ src/app/(pages)/(public)/login/page.tsx
@@ -1,6 +1,6 @@
 import KakaoLoginButton from '@/shared/auth/components/KakaoLoginButton';
 
-export default function onBoadingPage() {
+export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <KakaoLoginButton />
