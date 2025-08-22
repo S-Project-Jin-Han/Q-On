dcaf1dd0f8833676f75cec3015905beb4fc0b30d
kuru2141 <kuru2141@naver.com>
2025-08-22 13:58:35 +0900

    #34 feat: Button 컴포넌트 구현 및 global.css 수정

---
+++ src/shared/ui/Button/buttonVariants.tsx
@@ -0,0 +1,31 @@
+import { cva } from 'class-variance-authority';
+
+export const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-primary text-primary-foreground shadow-xs hover:bg-[--color-primary-hover] active:bg-[--color-primary-active] focus-visible:ring-primary',
+        destructive:
+          'bg-error text-error-foreground shadow-xs hover:bg-red-600 active:bg-red-700 focus-visible:ring-error',
+        outline:
+          'border border-border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground shadow-xs hover:bg-[--color-secondary-hover] active:bg-[--color-secondary-active]',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
+      },
+      size: {
+        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        icon: 'size-9',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
